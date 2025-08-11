0d187293d81b9cc5efe4af499851aca7376b43c8
muhammadhabib16 <2411522024_muhammad@student.unand.ac.id>
2025-08-11 21:03:31 +0700

    memperbaiki sisa merge branch feat/kategori-filter dengan branch feat/autentikasi-rebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,32 @@
-import Homepage from "./feat/Homepage";
-import Kategori from "./feat/Kategori";
-import DetailProduk from "./feat/DetailProduk";
+import Homepage from "./pages/feat/Homepage";
+import Kategori from "./pages/feat/Kategori";
+import DetailProduk from "./pages/feat/DetailProduk";
 import Layout from "./components/Layout";
-import ShopCart from "./feat/ShopCart";
+import ShopCart from "./pages/feat/ShopCart";
+import ConfirmCode from "./pages/auth/ConfirmCode";
+import ForgotPassword from "./pages/auth/ForgotPassword";
+import Login from "./pages/auth/Login";
+import Register from "./pages/auth/Register";
+import ResetPassword from "./pages/auth/ResetPassword";
 import { Routes, Route, Link } from "react-router-dom";
-import Wishlist from "./feat/Wishlist";
+import Wishlist from "./pages/feat/Wishlist";
 
 export default function App() {
   return (
     <Routes>
-      <Route path="/" element={<Homepage />} />
-
+      <Route path="/" element={<Login />} />
+      <Route path="/homepage" element={<Homepage />} />
       <Route element={<Layout />}>
         <Route path="/kategori" element={<Kategori />} />
         <Route path="/kategori/:productId" element={<DetailProduk />} />
         <Route path="/wishlist" element={<Wishlist />} />
         <Route path="/shopcart" element={<ShopCart />} />
       </Route>
+
+      <Route path="/confirmcode" element={<ConfirmCode />}></Route>
+      <Route path="/forgotpassword" element={<ForgotPassword />}></Route>
+      <Route path="/register" element={<Register />}></Route>
+      <Route path="/resetpassword" element={<ResetPassword />}></Route>
 
       {/* <Route path="*" element={<NotFoundPage />} /> */}
     </Routes>
